e41e3c8a5274b0bf23acc5a170e8e2f71ee99092
sirshakespeare25 <sirshakespeare25@gmail.com>
2022-09-03 11:55:59 -0500

    added function that creates a user specified amount of divs

--- eas.js
+++ eas.js
@@ -1,5 +1,9 @@
-function mkGrid () {
-    const newDiv = document.createElement('div');
-    
+function mkGrid (input) {
+    const num = input;
+    for (let i = 0; i < num; i++) {
+        const container = document.querySelector('#container');
+        const newDiv = document.createElement('div');
+        container.appendChild(newDiv);
+    };
 };
-
+mkGrid(prompt('enter a number!'));
