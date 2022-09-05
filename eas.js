86f59a04ba4a1636e8bd57ebeb8682684f1c2fb8
sirshakespeare25 <sirshakespeare25@gmail.com>
2022-09-05 11:35:00 -0500

    re commit of items missing from last commit

--- eas.js
+++ eas.js
@@ -1,9 +1,24 @@
 function mkGrid (input) {
-    const num = input;
+    const num = (input );
     for (let i = 0; i < num; i++) {
         const container = document.querySelector('#container');
         const newDiv = document.createElement('div');
+        newDiv.classList.add('column');
         container.appendChild(newDiv);
-    };
+    }; 
+    mkRow(num);
+
 };
+function mkRow (input) {
+    const num = (input);
+        const container = document.querySelectorAll('.column');
+        container.forEach(row => {
+            for (i = 0; i < num; i++) {
+                const newDiv = document.createElement('div');
+                newDiv.classList.add('row');
+                row.appendChild(newDiv);
+            };
+        });
+};
+
 mkGrid(prompt('enter a number!'));
