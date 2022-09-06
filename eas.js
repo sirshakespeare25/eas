418344b37206c44854a84e463bb5e3e35ddd1982
sirshakespeare25 <sirshakespeare25@gmail.com>
2022-09-06 16:56:52 -0500

    set up the hover function, and added a functional reset button that is capped at 100 as an input number.

--- eas.js
+++ eas.js
@@ -1,15 +1,20 @@
-function mkGrid (input) {
+const reset = document.querySelector('.btn');
+reset.addEventListener('click', resetBoard);
+
+function mkRow (input) {
     const num = (input );
+    if (num > 100) { (mkRow(prompt('Number must be under 100!'))); return;};
     for (let i = 0; i < num; i++) {
         const container = document.querySelector('#container');
         const newDiv = document.createElement('div');
         newDiv.classList.add('column');
         container.appendChild(newDiv);
     }; 
-    mkRow(num);
+    mkGrid(num);
 
 };
-function mkRow (input) {
+
+function mkGrid (input) {
     const num = (input);
         const container = document.querySelectorAll('.column');
         container.forEach(row => {
@@ -19,6 +24,27 @@ function mkRow (input) {
                 row.appendChild(newDiv);
             };
         });
+        activateRows()
 };
 
-mkGrid(prompt('enter a number!'));
+function resetBoard (e) {
+    const columns = document.querySelectorAll('.column');
+    const rows = document.querySelectorAll('.row');
+    columns.forEach(column => {
+        column.remove()
+    });
+    rows.forEach(row => {
+        row.remove()
+    });
+    mkRow (prompt('enter a number!'));
+}
+
+    mkRow(16)
+
+function activateRows() {
+    const boxes = document.querySelectorAll('.row')
+    boxes.forEach(box => {
+        box.addEventListener('mouseover', () => 
+        box.classList.add('highlight')
+        )});
+};
